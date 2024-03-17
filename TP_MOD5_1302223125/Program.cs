3d66fe2a84ee24677ea6b2d01c97b17496100999
atha <athallah.az@gmail.com>
2024-03-17 22:37:45 +0700

    menambahkan class DataGeneric

--- TP_MOD5_1302223125/Program.cs
+++ TP_MOD5_1302223125/Program.cs
@@ -12,5 +12,19 @@ public class HaloGeneric
         HaloGeneric halo = new HaloGeneric();
         string nim = "1302223125";
         halo.SapaUser(nim);
+        DataGeneric<string> data = new DataGeneric<string>(nim);
+        data.PrintData();
+    }
+}
+public class DataGeneric<T>
+{
+    private T data;
+    public DataGeneric(T data)
+    {
+        this.data = data;
+    }
+    public void PrintData()
+    {
+        Console.WriteLine("Data yang tersimpan adalah: " + data);
     }
 }
--- TP_MOD5_1302223125/obj/Debug/net8.0/TP_MOD5_1302223125.AssemblyInfo.cs
+++ TP_MOD5_1302223125/obj/Debug/net8.0/TP_MOD5_1302223125.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TP_MOD5_1302223125")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e783a7794c8c03b77213d0a55f68a4966b02c10")]
 [assembly: System.Reflection.AssemblyProductAttribute("TP_MOD5_1302223125")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TP_MOD5_1302223125")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
